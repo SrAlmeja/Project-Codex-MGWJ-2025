9e9e6f610fbeb987b6f27b74a70603c50edc0ac5
SrAlmeja <sr.almeja@hotmail.com>
2025-07-31 22:47:10 -0600

    Starting Persisntant player and Additive Scene

---
+++ UV/Assets/AlmejaStuff/01Script/Scene/SceneGroupManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Systems.SceneManagment
+{
+    public class SceneGroupManager
+    {
+        public event Action<string> OnSceneLoaded = delegate { };
+        public event Action<string> OnSceneUnloaded = delegate { };
+        public event Action OnSceneGroupLoaded = delegate { };
+
+        SceneGroup ActiveSceneGroup;
+
+        public async Task LoadScenes(SceneGroup group, IProgress<float> progress, bool reloadDupScenes = false) {}
+        public async Task UnloadScenes() {}
+    }
+
+    public readonly struct AsyncOperationGroup
+    {
+        public readonly List<AsyncOperation> Operations;
+
+        public float Progress => Operations.Count == 0 ? 0 : Operations.Average(o => o.progress);
+        public bool IsDone => Operations.All(o => o.isDone);
+
+        public AsyncOperationGroup(int initialCapacity)
+        {
+            Operations = new List<AsyncOperation>(initialCapacity);
+        }
+}
+}
