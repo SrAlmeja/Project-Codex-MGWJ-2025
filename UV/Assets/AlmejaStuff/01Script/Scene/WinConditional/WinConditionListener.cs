64c136a382382de679f47871272a67635fd70852
SrAlmeja <sr.almeja@hotmail.com>
2025-09-19 01:46:13 -0600

    WinConditionalsInTesting

--- UV/Assets/AlmejaStuff/01Script/Scene/WinConditional/WinConditionListener.cs
+++ UV/Assets/AlmejaStuff/01Script/Scene/WinConditional/WinConditionListener.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class WinConditionListener : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private WinConditionalSet conditionSet;
+    [SerializeField] private string conditionID;
+
+    public void TriggerCondition()
     {
-        
+        conditionSet.MarkCompleted(conditionID);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
